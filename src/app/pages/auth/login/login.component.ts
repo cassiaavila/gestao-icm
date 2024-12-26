2abfb2d85b2f27bcb4394f59ee139a39e759233b
Aparecida de Cassia <sdcassia2@gmail.com>
2024-12-26 18:34:26 -0300

    Create validation

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,62 +1,108 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../service/auth.service';
 
+
+interface SignupObj {
+  userName: string,
+  email: string,
+  password: string,
+}
+
 @Component({
   selector: 'app-login',
   standalone: false,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'] // Corrigido para styleUrls
 })
-export class LoginComponent implements  OnInit {
+export class LoginComponent implements OnInit {
 
-  signUpUsers: any[]=[];
+  signUpUsers: any[] = [];
 
   signupObj: any = {
     userName: '',
     email: '',
-    password: ''
+    password: '',
 
   };
-  loginObj:any= {
+  loginObj: any = {
+    userName: '',
     email: '',
     password: '',
 
   }
 
-  constructor(private authService: AuthService) {}
-
-  onSubmit() {
-
+  constructor(private authService: AuthService) {
   }
 
   ngOnInit(): void {
     const localData = localStorage.getItem('signUpUsers');
-    if(localData != null) {
+    if (localData != null) {
       this.signUpUsers = JSON.parse(localData);
     }
+    console.log(this.signUpUsers);
   }
-  onSignUp(){
+
+  onSignUp() {
+    debugger
+    if (!this.validation(this.signupObj)) {
+      return;
+    }
     this.signUpUsers.push(this.signupObj);
-    //localStorage.setItem('username', this.signUpUsers[0].username);
     localStorage.setItem('signUpUsers', JSON.stringify(this.signUpUsers));
     console.log(this.signupObj);
     this.signupObj = {
       userName: '',
       email: '',
       password: '',
-
     };
 
   }
-  onLogin(){
-    console.log()
-    //debugger
-    const isUserExist = this.signUpUsers.find(m => m.userName === this.loginObj.userName && m.password == this.loginObj.password);
-    if(isUserExist != undefined){
+
+  onLogin() {
+    const isUserExist = this.signUpUsers.find(
+      m => m.email === this.loginObj.email && m.password === this.loginObj.password
+    );
+    if (isUserExist != undefined) {
       alert('User Login Successfully');
     } else {
-      alert( 'Wrong credentials.' );
+      alert('Wrong credentials.');
     }
   }
 
+  validation(data: SignupObj): boolean {
+    if (!data.userName || data.userName.trim().length < 1) {
+      alert('userName cannot be empty.');
+      return false;
+    }
+    if (!data.email || data.email.trim().length < 1) {
+      alert('email cannot be empty.');
+      return false;
+    }
+    if (!data.email.includes('@')) {
+      alert('email cannot be invalid.');
+      return false;
+    }
+    const email = data.email.split('@');
+    if (email[0].length < 1 || email[1].length < 1 || !email[1].includes('.com')) {
+      alert('email cannot be invalid.');
+      return false;
+    }
+    if (email[1].replace('.com', '').length < 1) {
+      alert('email cannot be invalid.');
+      return false;
+    }
+    if (this.signUpUsers.find(m => m.email === this.signupObj.email)) {
+      alert('email already exists.');
+      return false;
+    }
+    if (!data.password || data.password.trim().length < 1) {
+      alert('password cannot be empty.');
+      return false;
+    }
+    if (data.password.trim().length < 8) {
+      alert('password can have at least 8 characters long.');
+      return false;
+    }
+    return true;
+  }
 }
