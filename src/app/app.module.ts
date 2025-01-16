4def8ff93af5e4d039c8136be10408fd740222c5
Laércio Ávila <laercio@devtrack.com.br>
2025-01-16 18:14:06 -0300

    fix primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { LoginRegisterComponent } from './pages/auth/login-register/login-register.component'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { AccountComponent } from './pages/account/account.component'
-import { CommonModule, NgClass } from '@angular/common'
+import { CommonModule } from '@angular/common'
 import { HttpClientModule } from '@angular/common/http'
 import { ForgotPasswordResetComponent } from './pages/auth/forgot-password-reset/forgot-password-reset.component'
 import { HomeComponent } from './pages/home/home.component'
@@ -14,6 +13,8 @@ import { PaginatorModule } from 'primeng/paginator'
 import { MessageService, SharedModule } from 'primeng/api'
 import { TableModule } from 'primeng/table'
 import { ToastModule } from 'primeng/toast'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { SnotifyService, ToastDefaults } from 'ng-alt-snotify'
 
 @NgModule({
   declarations: [
@@ -32,10 +33,15 @@ import { ToastModule } from 'primeng/toast'
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
     ToastModule,
-    HttpClientModule
   ],
-  providers: [MessageService],
+  providers: [
+    MessageService,
+    { provide: 'SnotifyToastConfig', useValue: ToastDefaults },
+    SnotifyService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
